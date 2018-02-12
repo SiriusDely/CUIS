cf59bc2583c2ea88d225b6aa13b4355f79555cdf
Sirius Dely <siriusdely@icloud.com>
2018-02-12 14:34:11 +0700

    add_username_to_users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,28 @@ class User < ApplicationRecord
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :trackable, :validatable
   devise :database_authenticatable, :trackable, :validatable
+
+  attr_accessor :login
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  # Only allow letter, number, underscore and punctuation.
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validate :validate_username
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username)
+      where(conditions.to_h).first
+    end
+  end
+
+  protected
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
