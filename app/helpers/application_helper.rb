a7a0896663c20b371e9f5592d2ece60b7c2a801d
Sirius Dely <siriusdely@icloud.com>
2018-02-12 19:20:22 +0700

    improve login page's layout & css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def foundation_flash_class(level)
+    case level.to_sym
+    when :notice    then "callout primary"
+    when :alert     then "callout warning"
+    when :success   then "callout success"
+    when :info      then "callout info"
+    when :error     then "callout alert"
+    when :secondary then "callout secondary"
+    else "callout"
+    end
+  end
+
 end
