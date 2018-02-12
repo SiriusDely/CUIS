cfe805ae2086a0a442ed611247168461a68b8aa6
Sirius Dely <siriusdely@icloud.com>
2018-02-12 14:01:59 +0700

    only use devise's authenticatable, trackable, & validatable; add some trial tests

---
+++ test/system/home_test.rb
@@ -0,0 +1,8 @@
+require "application_system_test_case"
+
+class HomeTest < ApplicationSystemTestCase
+  test "visiting the index" do
+    visit home_index_url
+    assert_selector "h1", text: "Home"
+  end
+end
